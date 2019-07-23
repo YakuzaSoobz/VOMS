99f53d09b8c9028acd44260258ef1ddcb3b668a9
Yashin <ysoobramoney@gmail.com>
2019-07-23 21:16:23 +0200

    Tester
    CalcApp

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.num1txt = new System.Windows.Forms.TextBox();
+            this.num2txt = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.MultiplyButton = new System.Windows.Forms.Button();
+            this.DivideButton = new System.Windows.Forms.Button();
+            this.SubtractButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ansTxt = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // num1txt
+            // 
+            this.num1txt.Location = new System.Drawing.Point(164, 75);
+            this.num1txt.Name = "num1txt";
+            this.num1txt.Size = new System.Drawing.Size(133, 22);
+            this.num1txt.TabIndex = 0;
+            // 
+            // num2txt
+            // 
+            this.num2txt.Location = new System.Drawing.Point(457, 75);
+            this.num2txt.Name = "num2txt";
+            this.num2txt.Size = new System.Drawing.Size(133, 22);
+            this.num2txt.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.MultiplyButton);
+            this.groupBox1.Controls.Add(this.DivideButton);
+            this.groupBox1.Controls.Add(this.SubtractButton);
+            this.groupBox1.Controls.Add(this.AddButton);
+            this.groupBox1.Location = new System.Drawing.Point(164, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(426, 187);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // MultiplyButton
+            // 
+            this.MultiplyButton.Location = new System.Drawing.Point(44, 113);
+            this.MultiplyButton.Name = "MultiplyButton";
+            this.MultiplyButton.Size = new System.Drawing.Size(143, 43);
+            this.MultiplyButton.TabIndex = 3;
+            this.MultiplyButton.Text = "Multiply";
+            this.MultiplyButton.UseVisualStyleBackColor = true;
+            this.MultiplyButton.Click += new System.EventHandler(this.MultiplyButton_Click);
+            // 
+            // DivideButton
+            // 
+            this.DivideButton.Location = new System.Drawing.Point(242, 113);
+            this.DivideButton.Name = "DivideButton";
+            this.DivideButton.Size = new System.Drawing.Size(140, 43);
+            this.DivideButton.TabIndex = 2;
+            this.DivideButton.Text = "Divide";
+            this.DivideButton.UseVisualStyleBackColor = true;
+            this.DivideButton.Click += new System.EventHandler(this.DivideButton_Click);
+            // 
+            // SubtractButton
+            // 
+            this.SubtractButton.Location = new System.Drawing.Point(242, 48);
+            this.SubtractButton.Name = "SubtractButton";
+            this.SubtractButton.Size = new System.Drawing.Size(140, 45);
+            this.SubtractButton.TabIndex = 1;
+            this.SubtractButton.Text = "Subtract";
+            this.SubtractButton.UseVisualStyleBackColor = true;
+            this.SubtractButton.Click += new System.EventHandler(this.SubtractButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(44, 48);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(143, 45);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ansTxt
+            // 
+            this.ansTxt.Location = new System.Drawing.Point(164, 357);
+            this.ansTxt.Name = "ansTxt";
+            this.ansTxt.Size = new System.Drawing.Size(133, 22);
+            this.ansTxt.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 441);
+            this.Controls.Add(this.ansTxt);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.num2txt);
+            this.Controls.Add(this.num1txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox num1txt;
+        private System.Windows.Forms.TextBox num2txt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button MultiplyButton;
+        private System.Windows.Forms.Button DivideButton;
+        private System.Windows.Forms.Button SubtractButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox ansTxt;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ans;
+            double num1Parse;
+            double num2Parse;
+            bool success1 = double.TryParse(num1txt.Text, out num1Parse);
+            if (success1)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Incorrect values entered");
+            }
+            bool success2 = double.TryParse(num2txt.Text, out num2Parse);
+            if (success2)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("You fucking idiot");
+            }
+            ans = num1Parse + num2Parse;
+            ansTxt.Text = ans.ToString();
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+            double ans;
+            ans = (double.Parse(num1txt.Text)) - (double.Parse(num2txt.Text));
+            ansTxt.Text = ans.ToString();
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            double ans;
+            ans = (double.Parse(num1txt.Text)) * (double.Parse(num2txt.Text));
+            ansTxt.Text = ans.ToString();
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            double ans;
+            ans = (double.Parse(num1txt.Text)) / (double.Parse(num2txt.Text));
+            ansTxt.Text = ans.ToString();
+        }
+    }
+}
