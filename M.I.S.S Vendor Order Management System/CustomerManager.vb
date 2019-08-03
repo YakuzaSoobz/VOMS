2cfc17aad487550c109bf1fb2a34d85b156564e4
Yashin Soobramoney <ysoobramoney@gmail.com>
2019-08-03 22:29:05 +0200

    Customer Manager
    -Added Validity Checkers
    -Added Highlight System
    -Also added a new bug fix to a new problem to both
    customer manager and product manager

--- M.I.S.S Vendor Order Management System/CustomerManager.vb
+++ M.I.S.S Vendor Order Management System/CustomerManager.vb
@@ -81,24 +81,97 @@ Public Class CustomerManager
         End Try
     End Sub
 
-    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+    Public Function ValidateEmail(ByVal strCheck As String) As Boolean
         Try
-            Dim ret As Integer = MsgBox("Confirm changes?", vbYesNo)
-
-            If ret = 6 Then 'if user clicks yes to update'
-                CustomerBindingSource.EndEdit()
-                CustomerTableAdapter.Update(Group16DataSet)
-                MsgBox("Update successful!")
-                Call ButtonRefresh_Click(sender, e)
-            End If
-        Catch ex As SqlException
-            MsgBox("Cannot Update!", vbExclamation, "Cannot Update!")
-        Catch ex As NoNullAllowedException
-            MsgBox("Incorrect input!Follow correct format!", vbExclamation, "Incorrect Input")
+            Dim vEmailAddress As New System.Net.Mail.MailAddress(strCheck)
         Catch ex As Exception
-            MsgBox("Oops something went wrong!", vbExclamation, "Error!")
+            Return False
         End Try
+        Return True
+    End Function
+
+    Private Sub UpdateButton_Click(sender As Object, e As EventArgs) Handles UpdateButton.Click
+        If (FNameTextBox.Text = Nothing) Then
+            MsgBox("First Name field cannot be left blank!", vbOK)
+            FNameTextBox.ResetText()
+        ElseIf (SNameTextBox.Text = Nothing) Then
+            MsgBox("Surname field cannot be left blank!", vbOK)
+            SNameTextBox.ResetText()
+        ElseIf (DOBDateTimePicker.Text = Nothing) Then
+            MsgBox("DOB field cannot be left blank!", vbOK)
+            DOBDateTimePicker.ResetText()
+        ElseIf (ContactNumberMaskedTextBox.Text = "(   )") Then
+            MsgBox("Contact Number field cannot be left blank!", vbOK)
+            ContactNumberMaskedTextBox.ResetText()
+        ElseIf (EmailTextBox.Text = Nothing) Then
+            MsgBox("Email field cannot be left blank!", vbOK)
+            EmailTextBox.ResetText()
+        ElseIf (PasswordTextBox.Text = Nothing) Then
+            MsgBox("Password field cannot be left blank!", vbOK)
+            PasswordTextBox.ResetText()
+        ElseIf (StreetAddress1TextBox.Text = Nothing) Then
+            MsgBox("Street Address 1 field cannot be left blank!", vbOK)
+            StreetAddress1TextBox.ResetText()
+        ElseIf (SuburbTextBox.Text = Nothing) Then
+            MsgBox("Suburb field cannot be left blank!", vbOK)
+            SuburbTextBox.ResetText()
+        ElseIf (CityTextBox.Text = Nothing) Then
+            MsgBox("City field cannot be left blank!", vbOK)
+            CityTextBox.ResetText()
+        ElseIf (ProvinceTextBox.Text = Nothing) Then
+            MsgBox("Province field cannot be left blank!", vbOK)
+            ProvinceTextBox.ResetText()
+        ElseIf (PostalCodeMaskedTextBox.Text.Contains(" ")) Then
+            MsgBox("Postal Code field cannot be left blank!", vbOK)
+            PostalCodeMaskedTextBox.ResetText()
+        ElseIf (ActiveStatusComboBox.Text = Nothing) Then
+            MsgBox("Active Status field cannot be left blank!", vbOK)
+            ActiveStatusComboBox.ResetText()
+
+            'Above is a full check for blanks , contact num code is tweaked abit for masking'
+
+        ElseIf Not (ValidateEmail(EmailTextBox.Text)) Then
+            MsgBox("Email format is invalid! , Follow this format eg.starplatinum@example.com", vbOK)
+            EmailTextBox.ResetText() 'Email validator which calls function'
+        ElseIf (PasswordTextBox.Text.Length > 10 Or PasswordTextBox.Text.Length < 0) Then
+            MsgBox("Password length invalid!", vbOK)
+            PasswordTextBox.ResetText() 'Password length checker'
+        ElseIf Not (ActiveStatusComboBox.Text = "F" Or ActiveStatusComboBox.Text = "T") Then
+            MsgBox("Active Status field must be either T or F!", vbOK)
+            ActiveStatusComboBox.ResetText() 'Active Status checker'
+
+
+        Else
+            FNameTextBox.Text = FNameTextBox.Text.Trim
+            SNameTextBox.Text = SNameTextBox.Text.Trim
+            ContactNumberMaskedTextBox.Text = ContactNumberMaskedTextBox.Text.Trim
+            EmailTextBox.Text = EmailTextBox.Text.Trim
+            PasswordTextBox.Text = PasswordTextBox.Text.Trim
+            StreetAddress1TextBox.Text = StreetAddress1TextBox.Text.Trim
+            SuburbTextBox.Text = SuburbTextBox.Text.Trim
+            CityTextBox.Text = CityTextBox.Text.Trim
+
+            'Removes white spaces before and after certain fields to avoid invalids'
+
+            Try
+                Dim ret As Integer = MsgBox("Confirm changes?", vbYesNo)
+
+                If ret = 6 Then 'if user clicks yes to update'
+                    CustomerBindingSource.EndEdit()
+                    CustomerTableAdapter.Update(Group16DataSet)
+                    MsgBox("Update successful!")
+                    Call ButtonRefresh_Click(sender, e)
+                End If
+            Catch ex As SqlException
+                MsgBox("Cannot Update!", vbExclamation, "Cannot Update!")
+            Catch ex As NoNullAllowedException
+                MsgBox("Incorrect input!Follow correct format!", vbExclamation, "Incorrect Input")
+            Catch ex As Exception
+                MsgBox("Oops something went wrong!", vbExclamation, "Error!")
+            End Try
+        End If
     End Sub
+
 
     Private Sub ArchiveButton_Click(sender As Object, e As EventArgs) Handles ArchiveButton.Click
         ActiveStatusComboBox.SelectedItem = "F"
@@ -300,5 +373,89 @@ Public Class CustomerManager
 
     Private Sub BackButton_MouseHover(sender As Object, e As EventArgs) Handles BackButton.MouseHover
         CustomerManagerTip.SetToolTip(BackButton, "Click to go back")
+    End Sub
+
+    Private Sub FNameTextBox_TextChanged(sender As Object, e As EventArgs) Handles FNameTextBox.TextChanged
+        If (FNameTextBox.Text = Nothing) Then
+            FNameTextBox.BackColor = Color.MistyRose
+        Else
+            FNameTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub SNameTextBox_TextChanged(sender As Object, e As EventArgs) Handles SNameTextBox.TextChanged
+        If (SNameTextBox.Text = Nothing) Then
+            SNameTextBox.BackColor = Color.MistyRose
+        Else
+            SNameTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub ContactNumberMaskedTextBox_TextChanged(sender As Object, e As EventArgs) Handles ContactNumberMaskedTextBox.TextChanged
+        If (ContactNumberMaskedTextBox.Text = "(   )") Then
+            ContactNumberMaskedTextBox.BackColor = Color.MistyRose
+        Else
+            ContactNumberMaskedTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub EmailTextBox_TextChanged(sender As Object, e As EventArgs) Handles EmailTextBox.TextChanged
+        If (EmailTextBox.Text = Nothing) Then
+            EmailTextBox.BackColor = Color.MistyRose
+        ElseIf Not (ValidateEmail(EmailTextBox.Text)) Then
+            EmailTextBox.BackColor = Color.MistyRose
+        Else
+            EmailTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub PasswordTextBox_TextChanged(sender As Object, e As EventArgs) Handles PasswordTextBox.TextChanged
+        If (PasswordTextBox.Text = Nothing) Then
+            PasswordTextBox.BackColor = Color.MistyRose
+        Else
+            PasswordTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub StreetAddress1TextBox_TextChanged(sender As Object, e As EventArgs) Handles StreetAddress1TextBox.TextChanged
+        If (StreetAddress1TextBox.Text = Nothing) Then
+            StreetAddress1TextBox.BackColor = Color.MistyRose
+        Else
+            StreetAddress1TextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub SuburbTextBox_TextChanged(sender As Object, e As EventArgs) Handles SuburbTextBox.TextChanged
+        If (SuburbTextBox.Text = Nothing) Then
+            SuburbTextBox.BackColor = Color.MistyRose
+        Else
+            SuburbTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub CityTextBox_TextChanged(sender As Object, e As EventArgs) Handles CityTextBox.TextChanged
+        If (CityTextBox.Text = Nothing) Then
+            CityTextBox.BackColor = Color.MistyRose
+        Else
+            CityTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub ProvinceTextBox_TextChanged(sender As Object, e As EventArgs) Handles ProvinceTextBox.TextChanged
+        If (ProvinceTextBox.Text = Nothing) Then
+            ProvinceTextBox.BackColor = Color.MistyRose
+        Else
+            ProvinceTextBox.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub ActiveStatusComboBox_TextChanged(sender As Object, e As EventArgs) Handles ActiveStatusComboBox.TextChanged
+        If (ActiveStatusComboBox.Text = Nothing) Then
+            ActiveStatusComboBox.BackColor = Color.MistyRose
+        ElseIf Not (ActiveStatusComboBox.Text = "F" Or ActiveStatusComboBox.Text = "T") Then
+            ActiveStatusComboBox.BackColor = Color.MistyRose
+        Else
+            ActiveStatusComboBox.BackColor = Color.White
+        End If
     End Sub
 End Class
