ca18ae13558c6f16f81e8fb513b4b692e237f8aa
Shankar <sbdayal15@gmai.com>
2019-08-14 09:36:12 +0200

    Added 2 reports
    Products sold per month for last year and
    Products sold for each employee last year

---
+++ M.I.S.S Vendor Order Management System/AmountOfProductsSoldPerMonthForLastYear.vb
@@ -0,0 +1,9 @@
+﻿Public Class AmountOfProductsSoldPerMonthForLastYear
+	Private Sub AmountOfProductsSoldPerMonthForLastYear_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		ProductTableAdapter1.Fill(Group16DataSet1.Product)
+		Customer_QuoteTableAdapter1.Fill(Group16DataSet1.Customer_Quote)
+		Customer_Quote_Line_ItemTableAdapter1.Fill(Group16DataSet1.Customer_Quote_Line_Item)
+		quantityOfProductsSoldPerMonthForTheLastYear1.SetDataSource(Group16DataSet1)
+
+	End Sub
+End Class
